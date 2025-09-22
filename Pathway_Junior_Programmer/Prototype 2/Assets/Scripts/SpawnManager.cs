ba816a2aecf7fe57570a0b93477107ed5b89d751
baptiste amice <amice.bap@gmail.com>
2025-09-22 20:10:14 +0200

    pathway Junior Programmer : Lesson 2.4 - Collision Decisions

--- Pathway_Junior_Programmer/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Pathway_Junior_Programmer/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -8,18 +8,19 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 2.0f, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
-        
+
+    }
+    
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
