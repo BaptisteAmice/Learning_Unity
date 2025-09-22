f4c9f884f5a634ffc482ebe9ed3e628885c8d1a2
baptiste amice <amice.bap@gmail.com>
2025-09-22 15:49:15 +0200

    pathway Junior Programmer : Lesson 1.4

--- Pathway_Junior_Programmer/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Pathway_Junior_Programmer/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+
+    // Inputs
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+
+        // Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
