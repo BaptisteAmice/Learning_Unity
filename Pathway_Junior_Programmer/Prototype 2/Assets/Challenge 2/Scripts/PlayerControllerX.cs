cf779a9d0c8e510e20389dcb70fd6a4832e5a4db
baptiste amice <amice.bap@gmail.com>
2025-09-22 20:35:03 +0200

    pathway Junior Programmer : Challenge 2 - Play Fetch

---
+++ Pathway_Junior_Programmer/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 1.0f;
+    private float lastThrow = -10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastThrow + cooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastThrow = Time.time;
+        }
+    }
+}
